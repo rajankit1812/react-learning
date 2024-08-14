e0256ee1fca5652e44e904b97609d8706d90ccd8
rajankit1812 <rajankit120701@gmail.com>
2024-08-14 10:34:41 +0530

    interview question

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -5,15 +5,18 @@ import './App.css'
 
 function App() {
  //state ko change krne ke liye responsible
-  let [counter, setCounter] = useState(5)
+  let [counter, setCounter] = useState(5)  //usestate patches me updates bhjta hai
   
   //let counter = 5
   
   //problem coming in UI updation as counter is updating as a variable but not in UI as React library allows changes in variables etc but in UI it gives special methods called Hooks -> UI udation controlled by REACT
   const addValue = () => {
-    counter = counter + 1
-    setCounter((counter <= 20) ? counter : 20)
     // counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)    //yha hm previous state lerhe hai isliye update krparhe hai nhi to counter+1 krne se update nhi hogi value as patches me jti hai and ab ye callback function aagya to jb ek complete hoga tbhi aage ayga. here as we are taking previous state to ek jan kaam chhodega tbhi dusra krega n isliye ek execute hoga tbhi dusra hoga
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    
   }
 
   const removeValue = () => {
